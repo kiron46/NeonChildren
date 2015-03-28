d54edf757fe54a08f422b692610a1a5f5fe602b9
Cole O'Brien <obriencole11@gmail.com>
2015-03-28 15:05:34 -0400

    AddedMovementScript

---
+++ Assets/playerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	//Speed modifier
+	public float speed;
+	//horizontal input
+	float h;
+	//Vertical Input
+	float v;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.Rigidbody2D.AddForce(vector2.right * h);
+		gameObject.Rigidbody2D.AddForce(vector2.up * v);
+	}
+}
